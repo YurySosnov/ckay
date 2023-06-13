634d53425a5aec6945192dff01e123fe8aa749a9
Yury Sosnov <yury.sosnov@neklo.com>
2023-06-13 21:36:42 +0400

    Plan View Prepared

--- js/site.js
+++ js/site.js
@@ -222,6 +222,7 @@ const ClassBASE = function() {
     }
 
     /* CALCULATOR */
+
     this.prices = {
         hr: 100,
         ss: 0.2,
@@ -230,24 +231,28 @@ const ClassBASE = function() {
     }
     this.calcDefaults = {
         title: 'Title',
+        text: '',
         mt: 120,
         et: 30,
         ss: 50,
         cc: 50,
         rt: 5
     }
+
     this.calculatePrice = function () {
         let ls = window.localStorage;
         let p = this.prices;
 
-        let mt = $('._js-calc-mt').val() / 60;
-        let et = $('._js-calc-et').val() / 60;
+        let mt = $('._js-calc-mt').val();
+        let et = $('._js-calc-et').val();
+        let mth = mt / 60;
         let ss = $('._js-calc-ss').val();
         let cc = $('._js-calc-cc').val();
         let rt = $('._js-calc-rt').val();
         let title = $('._js-calc-title').val();
+        let text = $('._js-calc-text').val();
 
-        let mp = mt * p.hr + ss*p.ss + cc * p.cc + rt * p.rt;
+        let mp = mth * p.hr + ss*p.ss + cc * p.cc + rt * p.rt;
         let ep = mp / mt * et;
         mp = Math.floor(mp * .85 / 10) * 10;
         ep = Math.floor(ep / 10) * 10;
@@ -261,8 +266,11 @@ const ClassBASE = function() {
         ls.setItem('rt', rt.toString());
         ls.setItem('mp', mp.toString());
         ls.setItem('title', title.toString());
+        ls.setItem('text', text.toString());
     }
+
     this.encodeImageFileAsURL = function(element){
+        if (!element.files[0]) return false;
         let file = element.files[0];
         let reader = new FileReader();
         reader.onloadend = function () {
@@ -279,6 +287,7 @@ const ClassBASE = function() {
         });
         let d = this.calcDefaults;
         $('._js-calc-title').val(d.title);
+        $('._js-calc-text').val(d.text);
         $('._js-calc-mt').val(d.mt);
         $('._js-calc-et').val(d.et);
         $('._js-calc-ss').val(d.ss);
@@ -295,20 +304,202 @@ const ClassBASE = function() {
 
     /* RENDER PLAN */
 
-    this.renderPlan = function() {
+    this.plans = {
+        'Pilot': {
+            title: 'Pilot',
+            text: {
+                en: '',
+                ru: 'Если коротко, то это очень коротко. Это знакомство, начало, шаг вперёд\n'
+            },
+            image: 'pilot.jpg',
+            q: 1,
+            mt: 30,
+            ss: 10,
+            cc: 10,
+            rt: 1
+        },
+        'Portrait': {
+            title: 'Portrait',
+            text: {
+                en: '',
+                ru: 'Каждый человек это весь его путь, все переживания и достижения. Часто скрытые, глубокие черты характера личности возможно уловить за доли секунды'
+            },
+            image: 'portrait.jpg',
+            q: 1,
+            mt: 90,
+            ss: 100,
+            cc: 40,
+            rt: 5
+        },
+        'Product': {
+            title: 'Product',
+            text: {
+                en: '',
+                ru: 'Вкусный…Сочный…Нежный… Это все, что надо знать\n'
+            },
+            image: 'product.jpg',
+            q: 1,
+            mt: 60,
+            ss: 100,
+            cc: 40,
+            rt: 5
+        },
+        'Moment': {
+            title: 'Moment',
+            text: {
+                en: '',
+                ru: 'Задумчивый взгляд в очереди за бургером может рассказать о человеке не меньше, чем его портрет, который художник рисовал несколько дней\n'
+            },
+            image: 'moment.jpg',
+            q: 1,
+            mt: 90,
+            ss: 100,
+            cc: 40,
+            rt: 5
+        },
+        'Character': {
+            title: 'Character',
+            text: {
+                en: '',
+                ru: 'Человек стремится примерить новые образы и маски. Можно даже стать чем-то иным, за пределами человеческих ролей, чтобы насладиться свободой\n'
+            },
+            image: 'character.jpg',
+            q: 1,
+            mt: 120,
+            ss: 50,
+            cc: 25,
+            rt: 5
+        },
+        'Promo': {
+            title: 'Promo',
+            text: {
+                en: '',
+                ru: 'Презентации повсюду. Всё потому, что нам есть, что показать, есть чем гордиться, что предложить обществу и самим себе'
+            },
+            image: 'promo.jpg',
+            q: 1,
+            mt: 60,
+            ss: 50,
+            cc: 20,
+            rt: 5
+        },
+        'Family': {
+            title: 'Family',
+            text: {
+                en: '',
+                ru: 'Семья едва ли не самое прочное, что есть в человеческой природе. Семья помогает и воспитывает, утешает и делает сильнее'
+            },
+            image: 'family.jpg',
+            q: 1,
+            mt: 90,
+            ss: 100,
+            cc: 30,
+            rt: 3
+        },
+        'Adventure': {
+            title: 'Adventure',
+            text: {
+                en: '',
+                ru: 'В “приключении” мы отправимся по стопам истории. Пережить что-то невероятное, сказочное, захватывающее - желание любого человека\n'
+            },
+            image: 'adventure.jpg',
+            q: 1,
+            mt: 120,
+            ss: 50,
+            cc: 20,
+            rt: 5
+        },
+        'Nude': {
+            title: 'Nude',
+            text: {
+                en: '',
+                ru: 'Красота человеческого тела манит и предостерегает. Этот стиль как никакой другой помогает стать увереннее и полюбить себя'
+            },
+            image: 'nude.jpg',
+            q: 1,
+            mt: 120,
+            ss: 60,
+            cc: 30,
+            rt: 5
+        },
+        'The One': {
+            title: 'The One',
+            text: {
+                en: '',
+                ru: 'Сюжет, эмоция, мораль или идея, а может всё вместе, может 10 лет жизни будет витать в одном снимке'
+            },
+            image: 'the_one.jpg',
+            q: 2.5,
+            mt: 60,
+            ss: 1,
+            cc: 1,
+            rt: 1
+        },
+    }
+
+    this.renderPlan = function(plan) {
+        let container, data, image;
+        if (!!plan && typeof plan === 'object') {
+            if (plan.hasOwnProperty('container')) container = plan.container;
+            if (plan.hasOwnProperty('data')) data = plan.data;
+            if (plan.hasOwnProperty('image')) image = plan.image;
+        }
         let ls = window.localStorage;
-        let data = {
+        if (!container) container = $('#plan');
+        if (!data) data = {
             ss: ls.getItem('ss'),
             cc: ls.getItem('cc'),
             rt: ls.getItem('rt'),
             mt: ls.getItem('mt'),
             mp: ls.getItem('mp'),
+            text: ls.getItem('text'),
             title: ls.getItem('title'),
         }
-        let image = ls.getItem('image');
-        for (let key in data) $('._js-render-' + key).html(data[key]);
-        $('._js-render-image').css({'background-image' : `url(${image})`});
+        if (!image) image = ls.getItem('image');
+
+        for (let key in data) container.find('._js-render-' + key).html(data[key]);
+        container.find('._js-render-image').css({'background-image' : `url(${image})`});
     }
+    this.createPlansList = function() {
+        let plansHtml = '';
+        for (let key in this.plans) {
+            let plan = this.plans[key];
+            plansHtml += `<div class="plans-list-item _js-plans-list-item" data-key="${key}">${plan.title}</div>`;
+        }
+        $('._js-plans-list').html(plansHtml);
+        $(document).on('click', '._js-plans-list-item', function(){
+            let key = $(this).data('key');
+            _this.calculatePlan(_this.plans[key]);
+        });
+    }
+    this.calculate = function ({mt, ss, cc, rt, q}) {
+        let p = this.prices;
+        let mth = mt / 60;
+        let mp = mth * p.hr * q + ss * p.ss + cc * p.cc + rt * p.rt;
+        return {mp, mt, ss, cc, rt}
+    }
+    this.calculatePlan = function (plan) {
+        let planData = this.calculate(plan);
+        planData.title = plan.title;
+        planData.text = plan.text;
+        planData.image = plan.image;
+        window.localStorage.setItem('plan', JSON.stringify(planData));
+    }
+    this.renderPreparedPlan = function() {
+        let lang = 'ru';
+        let plan = JSON.parse(window.localStorage.getItem('plan'));
+        let container = $('#plan');
+        for (let key in plan) {
+            if (key !== 'image') {
+                container.find('._js-render-' + key).html(plan[key]);
+            }
+            if (key === 'text') {
+                container.find('._js-render-' + key).html(plan[key][lang]);
+            }
+        }
+        container.find('._js-render-image').css({'background-image': `url(/images/plans/${plan.image})`});
+    }
+
 };
 
 $(function(){
